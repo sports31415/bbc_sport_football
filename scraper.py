16639b4c7a9404fe7b487bdb50518d4ce0394772
sports31415 <will851@yahoo.com>
2015-11-24 15:04:17 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,17 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
+import scraperwiki
+import lxml.html
+
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://www.bbc.co.uk/sport/0/football/34824958")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+#root.cssselect("div[align='left']")
+print lxml.html.tostring(html)
+
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
